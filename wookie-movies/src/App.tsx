b4981ec9c4f64adbacfe1694a731a96ce4ca1fd7
Segopotso Makhutja <segopotso.makhutja@sovtech.com>
2022-04-02 19:38:22 +0200

    initial setup, remove unnecessary files - test codesubmit

---
+++ wookie-movies/src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+import {
+  ChakraProvider,
+  Box,
+  theme,
+} from "@chakra-ui/react"
+import { ColorModeSwitcher } from "./ColorModeSwitcher"
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+      <ColorModeSwitcher justifySelf="flex-end" />
+    </Box>
+  </ChakraProvider>
+)
