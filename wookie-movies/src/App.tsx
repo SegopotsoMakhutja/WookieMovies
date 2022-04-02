b72bc8488935c38f0788cbb74dd1c6467b8357c0
Segopotso Makhutja <segopotso.makhutja@sovtech.com>
2022-04-02 20:17:00 +0200

    setup ESLint, Prettier and TS rules for best practices

--- wookie-movies/src/App.tsx
+++ wookie-movies/src/App.tsx
@@ -1,15 +1,15 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable import/prefer-default-export */
+import * as React from 'react';
+import { ChakraProvider, Box, theme } from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <ColorModeSwitcher justifySelf="flex-end" />
-    </Box>
-  </ChakraProvider>
-)
+export function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <ColorModeSwitcher justifySelf="flex-end" />
+      </Box>
+    </ChakraProvider>
+  );
+}
